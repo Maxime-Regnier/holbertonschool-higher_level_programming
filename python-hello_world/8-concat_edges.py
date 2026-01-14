883642fde770b10b9320b1b9fb4dc7f898df8a4a
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-14 17:50:25 +0100

    add line file

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming \
 language that combines remarkable power with very clear syntax"
-print(str[39:78] + " with Python")
+
+print(str[39:80] + " with Python")
