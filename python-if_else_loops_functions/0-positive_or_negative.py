d7654dfcd6585ce717a4b0a3b04aaef40b0951dc
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-15 15:14:38 +0100

    Add check for random number sign and print positive/zero/negative

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-1000, 1000)
+print(f"{number}", end=" ")
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
+else:
+    print("is negative")
