205c8b8fb60d1ec06e6108ccc57b5b874d17e7cc
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-15 16:40:20 +0100

    Add line end file

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-100, 100)
-number = 98
+number = random.randint(-10, 10)
 print(f"{number}", end=" ")
 if number > 0:
     print("is positive")
