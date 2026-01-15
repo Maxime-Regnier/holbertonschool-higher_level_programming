b2c35a78a759114ae27fd230c20853895c414045
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-15 15:46:26 +0100

    "Fix check to correctly identify positive, negative or zero"

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-1000, 1000)
 number = 98
 print(f"{number}", end=" ")
+number = -98
 if number > 0:
     print("is positive")
 elif number == 0:
