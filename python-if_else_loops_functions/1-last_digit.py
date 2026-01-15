bf1c36aec73d56d41a142cf58152fd33c8642573
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-15 16:22:39 +0100

    "commit: Fix last digit for negative numbers"

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,8 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = number % 10
+if number < 0 and last_digit != 0:
+    last_digit -= 10
 
 print(f"Last digit of {number} is {last_digit}", end=" ")
 
