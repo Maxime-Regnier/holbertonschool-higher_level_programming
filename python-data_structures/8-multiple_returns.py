0fd1d64b66cbcf75bd1a6a3dd13f09a39f5732bf
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-20 11:54:24 +0100

    "Fix multiple_returns: always define length before conditional"

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    length = len(sentence)
     if sentence == "":
         first_char = None
     else:
         first_char = sentence[0]
-        length = len(sentence)
+
     return(length, first_char)
