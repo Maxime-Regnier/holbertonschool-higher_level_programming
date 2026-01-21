a9e5bf81f6ac530bf08df1bc6969207ad6a906f7
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-21 10:48:20 +0100

    "Add uniq_add function to sum all unique integers in a list"

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_elements = []
+    for element in my_list:
+        if element not in unique_elements:
+            unique_elements.append(element)
+            total = 0
+            for element in unique_elements:
+                total += element
+    return total
