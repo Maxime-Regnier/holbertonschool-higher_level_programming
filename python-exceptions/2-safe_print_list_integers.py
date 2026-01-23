4a7166bd9979f19765770cc66e336e12676cb9f4
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-23 17:50:01 +0100

    "Fix safe_print_list_integers: correct indentation and function signature"

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
@@ -7,5 +7,5 @@ def safe_print_integer(value):
             count += 1
         except (ValueError, TypeError):
             pass
-            print()
-            return count
+    print()
+    return count
