8fbaf567ce5fa36d5823cdb52fedf1fbbb6b4c21
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-23 17:22:37 +0100

    "Add safe_print_integer function with try/except (incomplete)"

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     pass
-try:
-    pass
-except:
-    pass
-def safe_print_integer(value):
-    try:
-        pass
-    except:
-        pass
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except:
-        return False
-def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
