3f0461c6d69722db305f0c61fea39aad522bee03
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-23 17:34:11 +0100

    "Add safe_print_list_integers with try/except and integer-only printing"

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+            print()
+            return count
