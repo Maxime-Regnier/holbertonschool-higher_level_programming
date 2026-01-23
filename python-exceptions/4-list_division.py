24d60ff66629c12d0899226b1e673ff154b85e68
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-23 18:10:38 +0100

    "Clean list_division: remove unnecessary code and add newline at end of file"

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,8 +2,6 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
-        pass
-    for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except IndexError:
@@ -17,5 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
-        return new_list
+    return new_list
 
