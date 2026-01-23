9b7c2f52185ee95be7b1b141a2b08c204feaf172
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-23 22:41:15 +0100

    "Add matrix_divided function with input validation and doctests"

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that divides all elements of a matrix
+"""
+
+def matrix_divided(matrix, div):
+    # Vérification de matrix
+    if (not isinstance(matrix, list) or matrix == [] or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(isinstance(num, (int, float)) for row in matrix for num in row)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # Vérification que toutes les lignes ont la même taille
+    row_length = len(matrix[0])
+    if not all(len(row) == row_length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    # Vérification de div
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Nouvelle matrice
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(round(num / div, 2))
+        new_matrix.append(new_row)
+
+    return new_matrix
