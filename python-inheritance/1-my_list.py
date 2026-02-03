316082cfd3adbd07654c482f6134f4c756d273fb
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-03 16:17:09 +0100

    Add init and string definition

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,5 +3,11 @@
 class MyList(list):
     """ Arg: list
         No return """
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+    # Return a string representation of the object
+        pass
     def print_sorted(list):
         return print(sorted(list))
