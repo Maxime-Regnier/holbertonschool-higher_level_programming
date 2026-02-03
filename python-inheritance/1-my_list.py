7fcce356af8f10cbe2ada0d01ed37ae86419f11f
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-03 16:01:19 +0100

    Creation function sorted and print list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" prints the list, but sorted (ascending sort) """
+class MyList(list):
+    """ Arg: list
+        No return """
+    def print_sorted(list):
+        return print(sorted(list))
