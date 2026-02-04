39989be4393f4ca6488cf301679f767f7280a629
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-04 15:01:07 +0100

    Fix space 2-is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,7 @@ True if object is exactly an instance of the specified class
 False otherwise
 """
 
+
 def is_same_class(obj, a_class):
     """ funtion to test if object is exactly an instance of a class """
     if type(obj) is a_class:
