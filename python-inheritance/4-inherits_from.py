d3fa2983f626162689c96bf206c2c71d5f7e7d6b
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-04 16:23:15 +0100

    Fix test of type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ False otherwise
 
 def inherits_from(obj, a_class):
     """ funtion to test if object is an instance of a class """
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
