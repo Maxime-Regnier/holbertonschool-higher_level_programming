327e4f84b40cbc7a00f7ae68f96d6f32c2bbc23f
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-05 00:30:34 +0100

    "Create BaseGeometry class with area() and integer_validator() and add doctests"

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-""" Module containing the BaseGeometry class. """
+"""Module containing the BaseGeometry class."""
 
 
 class BaseGeometry:
-    """Base class for geometry"""
-    
+    """Base class for geometry."""
+
     def area(self):
-        """Raises an Exception because area is not implemented"""
+        """Raises an Exception because area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is a positive integer"""
+        """
+        Validates that value is a positive integer.
+
+        Args:
+            name (str): Name of the variable
+            value (any): Value to validate
+
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value <= 0
+        """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
