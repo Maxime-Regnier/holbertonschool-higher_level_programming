ee31470c960bf929939a6a6d55c4557c4990b1ac
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-04 16:46:32 +0100

    Create function area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Document containing the BaseGeometry class """
+
+
+class BaseGeometry:
+    """ Class that manages geometry parameters """
+    def area(self):
+        """ Function that handles exceptions for air calculation """
+        raise Exception("area() is not implemented")
