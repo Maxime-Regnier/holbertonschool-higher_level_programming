d84f948fbfd553a70d2637e908f5c54513f8cd51
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-04 22:48:54 +0100

    "Fix PEP8 style issues: remove trailing whitespaces and extra blank lines in 8-rectangle.py"

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,25 +7,25 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """
     Rectangle class inherits from BaseGeometry.
-    
+
     Attributes:
     __width (int): Width of the rectangle (private)
     __height (int): Height of the rectangle (private)
     """
-    
-    def __init__(self, width, height):   
+
+    def __init__(self, width, height):
         """
         Initialize a Rectangle instance.
-    
+
         Args:
             width (int): Width of the rectangle (must be positive integer)
             height (int): Height of the rectangle (must be positive integer)
         """
-        self.integer_validator("width", width)    
+        self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def __str__(self):
         """Return a readable string representation of the rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
