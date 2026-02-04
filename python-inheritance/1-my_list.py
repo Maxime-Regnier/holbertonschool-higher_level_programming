dc5788efa242744861818b63295f79045132fdd9
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-05 00:44:50 +0100

    "Task 1: MyList class with print_sorted() creates a new sorted list without modifying original list, handles negatives"

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,4 +7,6 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list in ascending order without modifying the original list."""
-        print(sorted(self))
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
