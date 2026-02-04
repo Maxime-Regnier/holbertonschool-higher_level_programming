d930478fc2e025a7a03458303214fd72bbd11ac9
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-05 00:18:14 +0100

    "Create Square class inheriting from Rectangle with size validation, area, and __str__ returning [Square] width/height"

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module defining a Square class inheriting from Rectangle."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle."""
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): size of the square (positive integer)
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        # Call Rectangle constructor with width=size, height=size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return the square description for print() and str()."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
