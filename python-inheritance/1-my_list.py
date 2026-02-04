1e6120856740564d0fdb2abd122b8bb2177ff046
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-04 12:12:49 +0100

    fix error class Mylist

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,11 +10,6 @@ class MyList(list):
     to print the list sorted in ascending order.
     """
 
-    def __str__(self):
-        """ We transform the instance (self) into a standard list,
-        then into a string to fit the expected format. """
-        return super().__str__()
-
     def print_sorted(self):
         """
         Prints the elements of the list in ascending order without
