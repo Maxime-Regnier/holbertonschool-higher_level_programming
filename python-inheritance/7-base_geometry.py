85836be219558cf13eea4891284c89da4b810bfb
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-04 17:27:54 +0100

    "Fix whitespace: remove trailing spaces in 7-base_geometry.py"

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
         """ Calculates the area of the shape. """
         raise Exception("area() is not implemented")
     
+
     def integer_validator(self, name, value):
         """ Validates that a value is a positive integer. """
         if type(value) is not int:
