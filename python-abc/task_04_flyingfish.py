0470c9eee09116f1be0b6d46a3b7d2473f0d608e
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-06 16:45:18 +0100

    create fish and bird

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+class Fish:
+    def swim(self):
+        print("The fish is swimming")
+    def habitat(self):
+        print("The fish lives in water")
+        
+class Bird:
+    def fly(self):
+        print("The bird is flying")
+    def habitat(self):
+        print("The bird lives in the sky")
