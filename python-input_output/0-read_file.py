076b815f714e8a192c0aeb34711c2d9cfcd1aaa9
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-09 17:25:46 +0100

    "Implement read_file to print UTF-8 file content"

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Reads a text file (UTF-8) and prints it to standard output."""
+
 def read_file(filename=""):
+    """Reads a text file and prints its content to standard output.
+     
+     Args:
+            filename (str): The name of the file to read
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
