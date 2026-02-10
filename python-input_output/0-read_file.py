26f4d7fb7c16554071ed2334cadc0322f1bcc748
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-10 11:53:32 +0100

    "Correct blank lines, trailing whitespace, and missing newline in 0-read_file.py"

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Reads a text file (UTF-8) and prints it to standard output."""
 
+
 def read_file(filename=""):
     """Reads a text file and prints its content to standard output.
-     
+
      Args:
             filename (str): The name of the file to read
     """
