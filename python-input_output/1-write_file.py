f45564a21fe21889f795776b931e3acbeccedc8a
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-10 15:39:44 +0100

    "Fix PEP8 style issues in 1-write_file.py"

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3    
-"""Writes a string to a UTF-8 text file and returns the number of characters written.
-    
+#!/usr/bin/python3
+"""Writes a string to a UTF-8 text file
+and returns the number of characters written.
+
 Args:
     filename (str): The name of the file
     text (str): The text to write
@@ -8,8 +9,11 @@ Args:
 Returns:
     int: Number of characters written
 """
+
+
 def write_file(filename="", text=""):
-    """Write text to a UTF-8 file and return the number of characters written."""
+    """Write text to a UTF-8 file
+    and return the number of characters written."""
 
     with open(filename, "w", encoding="utf-8") as f:
         chars_written = f.write(text)
