4b6497682e0e6b7d77f291bd93ed090e1d155230
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 22:34:17 +0100

    "Add save_to_json_file function and fix PEP8 issues (2 blank lines after import, remove trailing whitespace)"

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Module for saving a Python object to a file using JSON representation.
+"""
 import json
+
+
 def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file using a JSON representation.
+    """
     with open(filename, "w") as f:
         json.dump(my_obj, f)
-        
-    
