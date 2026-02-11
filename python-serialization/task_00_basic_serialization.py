3f530e7248b5ccda3cc1ab731ef43aab9104c539
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 18:46:16 +0100

    "Fix Check 1: serialize_and_save_to_file writes dictionary to JSON without extra print or return"

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,5 +4,3 @@ def serialize_and_save_to_file(data, filename):
     pass
     with open(filename, "w") as f:
         json.dump(data, f)
-        print(f"The data has been serialized and saved to '{filename}'")
-    return data
