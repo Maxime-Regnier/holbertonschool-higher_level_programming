6dfed4b5f3359cf706f10d4630dee94f2420d387
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 18:19:00 +0100

    git commit -m "Add placeholder for serialize_and_save_to_file function"

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import json
+def serialize_and_save_to_file(data, filename):
+    pass
+    with open(filename, "w") as f:
+        print(f"The data has been serialized and saved to '{filename}'")
+    return data
