a6fd7ac18d92e31c4a93b8d26fb65822a4214601
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 10:01:54 +0100

    "Fix PEP8 issues in 2-append_write.py"

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Append a string to a UTF-8 text file."""
-def append_write(filename="", text=""):    
-    """Append text to a UTF-8 file and return the number of characters added."""
 
-    with open (filename, "a", encoding="utf-8") as f:
+
+def append_write(filename="", text=""):
+    """Append text to a UTF-8 file and
+    return the number of characters added."""
+
+    with open(filename, "a", encoding="utf-8") as f:
         chars_written = f.write(text)
         return chars_written
