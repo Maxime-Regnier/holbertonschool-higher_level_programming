dd96d5da37a617092ee5a195a3d5c23f4a202a18
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 18:35:02 +0100

    "Fix indentation of return in serialize_and_save_to_file function"

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,4 +4,4 @@ def serialize_and_save_to_file(data, filename):
     pass
     with open(filename, "w") as f:
         print(f"The data has been serialized and saved to '{filename}'")
-        return data
+    return data
