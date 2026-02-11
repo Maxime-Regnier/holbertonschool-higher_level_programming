b01838a6c2105aa2e4148304c8ea9cdddd30d79e
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 22:21:20 +0100

    "Add basic JSON serialization and deserialization module"

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,3 +6,10 @@ def serialize_and_save_to_file(data, filename):
     """Serialize a Python dictionary to a JSON file."""
     with open(filename, "w") as f:
         json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """Load and deserialize a JSON file to a Python dictionary."""
+
+    with open(filename, "r") as f:
+        return json.load(f)
