f8072e50b072e481121df8e71f4585feb99e6d9c
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-11 18:52:13 +0100

    "Fix PEP8: add 2 blank lines after imports and remove trailing whitespace"

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import json
-def serialize_and_save_to_file(data, filename):    
+
+
+def serialize_and_save_to_file(data, filename):
     """Serialize a Python dictionary to a JSON file."""
     with open(filename, "w") as f:
         json.dump(data, f)
