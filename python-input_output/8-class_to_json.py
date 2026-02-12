889caee7372938c8dbdc606d6de03e281fc81c84
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-12 12:24:27 +0100

    "Fix PEP8 issues in 8-class_to_json.py: blank lines, line length, and EOF newline"

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,6 +3,9 @@
 Module that defines a function to return the dictionary
 description of an object for JSON serialization.
 """
+
+
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return the dictionary description of
+    an object for JSON serialization."""
     return obj.__dict__
