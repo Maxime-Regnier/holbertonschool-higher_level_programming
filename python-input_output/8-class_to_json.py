5cb094322f0f679efa8346f5bec9b316f9465017
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-12 12:22:19 +0100

    "Add module and function docstrings to class_to_json.py"

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module that defines a function to return the dictionary
+description of an object for JSON serialization.
+"""
 def class_to_json(obj):
     """Return the dictionary description of an object for JSON serialization."""
     return obj.__dict__
