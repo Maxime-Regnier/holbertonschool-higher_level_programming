38ac77a25a06aa6800029a41f28c26f87c38de88
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-13 15:39:49 +0100

    "Implement Student class with to_json and reload_from_json methods"

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Module defines Student class."""
+
+
+class Student:
+    """Student with first_name, last_name, and age."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize student attributes."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dict of attributes; filter by attrs if list provided."""
+        if isinstance(attrs, list):
+            filtered_dict = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    filtered_dict[key] = getattr(self, key)
+            return filtered_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
