1e5adfe53918238c18cd42decf5d8869a6703dd0
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-13 10:55:44 +0100

    "Fix PEP8 issues (blank lines and trailing whitespace)"

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a student."""
 
+
 class Student:
     def __init__(self, first_name, last_name, age):
         """Initialize student attributes."""
@@ -8,6 +9,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """Return dictionary representation of the Student instance."""
-        return self.__dict__    
+        return self.__dict__
