53b11afa4bc96d9f0bc63563c3a57dd4cf37b5d9
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-13 16:09:51 +0100

    Defined the pascal_triangle function to generate Pascal's triangle. Corrected a typo in the variable name (previouse_row → previous_row) that was causing a NameError.

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        previous_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, len(previous_row)):
+            new_row.append(previous_row[j - 1] + previous_row[j])
+        
+        new_row.append(1)
+        triangle.append(new_row)
+    
+    return triangle
