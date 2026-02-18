1b9d9bb817b8827722e027c9462571a0fcefaa1f
Maxime-Regnier <11493@holbertonstudents.com>
2026-02-18 06:48:02 +0100

    Add Flask API with user routes and POST handling

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+users = {}
+from flask import jsonify
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
+@app.route("/status")
+def status():
+    return "OK"
+@app.route("/users/<username>")
+def get_user(username):
+    if username in users:
+        return jsonify(user[username])
+    return jsonify({"error": "User not found"}), 404
+from flask import request
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid JSON"}), 400
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}),400
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 409
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
+if __name__ == "__main__":
+    app.run()
