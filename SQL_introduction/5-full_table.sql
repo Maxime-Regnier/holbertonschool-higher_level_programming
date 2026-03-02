5c6932b1c05c57c72135e6f4453cf9b1a3457e2b
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-02 16:28:44 +0100

    "Update query: add full column description fields and remove fixed database filter"

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,10 @@
 -- Print the column names and types of first_table
-SELECT column_name, column_type
+SELECT 
+COLUMN_NAME AS Field,
+column_type AD Type,
+IS_NULLABLE AS 'Null',
+COLUMN_KEY AS 'Key',
+COLUMN_DEFAULT AD 'Default',
+EXTRA AS Extra
 FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+WHERE table_name = 'first_table';
