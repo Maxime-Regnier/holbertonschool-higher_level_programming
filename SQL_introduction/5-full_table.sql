c512f2cbc91bb49de87762b11f9805b7d965380c
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-02 16:36:08 +0100

    "Fix 5-full_table.sql: use SHOW CREATE TABLE for full table description"

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,2 @@
--- Print the column names and types of first_table
-SELECT 
-COLUMN_NAME AS Field,
-column_type AD Type,
-IS_NULLABLE AS 'Null',
-COLUMN_KEY AS 'Key',
-COLUMN_DEFAULT AD 'Default',
-EXTRA AS Extra
-FROM information_schema.columns
-WHERE table_name = 'first_table';
+-- Print the full description of first_table
+SHOW CREATE TABLE first_table;
