e0052a4cb80b3f1a80ddc6f6dbf7005132bd26d1
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-03 18:42:02 +0100

    Update comments for clarity in 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,9 +2,11 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '01234';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY '56789';
 
+-- Grant privileges
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_1'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_2'@'localhost';
 
+-- Apply changes
 FLUSH PRIVILEGES;
 
 -- List privileges for user_0d_1 and user_0d_2
