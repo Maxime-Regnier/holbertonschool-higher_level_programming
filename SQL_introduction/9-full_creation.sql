3dfcc84072a1350a2b0d97f45f24379f5027ea09
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-03 10:31:03 +0100

    Add comment and create second_table with multiple rows

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name vARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'Georges', 8);
