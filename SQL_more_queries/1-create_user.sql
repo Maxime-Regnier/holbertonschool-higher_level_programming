fbe155d6311ead8216b0d675e459d60059fd0b53
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-03 18:10:33 +0100

    Add comments file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
+-- Create user user_0d_1 with all privileges if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
