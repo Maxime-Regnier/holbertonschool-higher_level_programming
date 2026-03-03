ca2fd958c825b504fe877280c72d84d8c6582adc
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-03 17:22:29 +0100

    Add comments file

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
+-- List privileges for user_0d_1 and user_0d_2
 SELECT user, host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv
 FROM mysql.user
 WHERE user IN ('user_0d_1', 'user_0d_2') AND host='localhost'
