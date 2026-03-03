999c9f0c11fae8fada00a6313fe377b3c656fd22
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-03 18:14:28 +0100

    Add missing semicolon to end SQL statement

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -10,4 +10,4 @@ FLUSH PRIVILEGES;
 -- List privileges for user_0d_1 and user_0d_2
 SELECT user, host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv
 FROM mysql.user
-WHERE user IN ('user_0d_1', 'user_0d_2') AND host='localhost'
+WHERE user IN ('user_0d_1', 'user_0d_2') AND host='localhost';
