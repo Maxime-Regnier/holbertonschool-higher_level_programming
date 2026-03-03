fab041b5f2e69b507dbf377c3961110ddded068d
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-03 17:32:30 +0100

    Add creation of users and grant privileges before listing them

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,3 +2,10 @@
 SELECT user, host, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv
 FROM mysql.user
 WHERE user IN ('user_0d_1', 'user_0d_2') AND host='localhost'
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '01234';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY '56789';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_1'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
