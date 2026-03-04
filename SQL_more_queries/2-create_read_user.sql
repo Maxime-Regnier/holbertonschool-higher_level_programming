9b6fc8f526851f8f514c54a571a4024428becc6b
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-04 10:07:35 +0100

    Add comments file

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,9 @@
+-- Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create user if it does not exist
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant only SELECT privilege on hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- Apply changes
 FLUSH PRIVILEGES;
