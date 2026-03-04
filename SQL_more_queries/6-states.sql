7dd6aed02e1228c83e9eafb361b747ba71901242
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-04 22:42:37 +0100

    Fix missing comma between columns in states table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 -- Create table states if it does not exist
 CREATE TABLE IF NOT EXISTS states (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256) NOT NULL
 );
