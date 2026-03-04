1dc1575424703610bb2091cd684ee834eedc72f9
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-04 22:51:09 +0100

    Add comments file

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,8 @@
+-- Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Select the database
 USE hbtn_0d_usa;
+-- Create table cities if it does not exist
 CREATE TABLE IF NOT EXISTS cities (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 state_id INT NOT NULL,
