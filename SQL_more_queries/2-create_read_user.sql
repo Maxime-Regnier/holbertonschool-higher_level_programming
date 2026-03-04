b7c1557e1c6965afc54d40e8e7eb088a3cdc4040
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-04 10:13:01 +0100

    "Remove FLUSH PRIVILEGES and IDENTIFIED BY from user creation script"

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,7 @@
 -- Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user if it does not exist
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 -- Grant only SELECT privilege on hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
--- Apply changes
-FLUSH PRIVILEGES;
