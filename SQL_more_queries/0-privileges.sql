1db8ded6d83a9e2e245bd247ea7131638b2e5f7b
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-05 11:54:30 +0100

    "Clean up SQL comments - simplify and remove redundancy"

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,14 @@
--- Create users if they do not exist and grant privileges
+-- Create users if they do not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '01234';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY '56789';
 
--- Grant privileges to user_0d_1
+-- Grant privileges
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_1'@'localhost';
--- Grant privileges to user_0d_2
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_2'@'localhost';
 
 -- Apply changes
 FLUSH PRIVILEGES;
 
--- Display privileges for user_0d_1
+-- Display privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Display privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
