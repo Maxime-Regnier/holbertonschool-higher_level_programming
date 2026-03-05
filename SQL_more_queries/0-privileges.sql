6abdfbcd587d46731845d7e2d0e5cfaf1ece54c6
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-05 11:59:47 +0100

    Delete REVOKE command

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,9 +2,6 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '01234';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY '56789';
 
--- Revoke all privileges from user_0d_1
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
-
 -- Grant privileges
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_1'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_2'@'localhost';
