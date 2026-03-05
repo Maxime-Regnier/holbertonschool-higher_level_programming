dbc09ab0413ca7d9bae3c680214f9de2b58a53ed
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-05 12:22:36 +0100

    Fix: List privileges of users user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,14 +1,3 @@
--- Create users if they do not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY '01234';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY '56789';
-
--- Grant privileges
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE ON *.* TO 'user_0d_2'@'localhost';
-
--- Apply changes
-FLUSH PRIVILEGES;
-
--- Display privileges
+-- List all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
