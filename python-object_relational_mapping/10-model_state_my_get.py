c5c2af7c9e11ad802d0c8cd7837b3643f2eb9ff3
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 21:21:28 +0100

    "fix: remove Base.metadata.create_all from 10-model_state_my_get.py"

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -13,21 +13,19 @@ if __name__ == "__main__":
     # Engine creation with mysql and mysqldb DBAPI
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    # Creating all classes in DB
-    Base.metadata.create_all(engine)
-
     # Creating Session and its instance
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # The Query
-    state = session.query(State).filter(State.name == argv[4]).first()
+    try:
+        # The Query
+        state = session.query(State).filter(State.name == argv[4]).first()
 
-    # Printing the result
-    if state is None:
-        print("Not found")
-    else:
-        print(state.id)
-
-    # Closing the session
-    session.close()
+        # Printing the result
+        if state is None:
+            print("Not found")
+        else:
+            print(state.id)
+    finally:
+        # Closing the session
+        session.close()
