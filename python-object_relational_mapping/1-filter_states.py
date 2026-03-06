c65702c3f2a455767f6bba9f45d28e3e3af80cce
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 22:44:19 +0100

    List all states with names starting with N ordered by id

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
-Usage: ./1-filter_states.py <username> <password> <database>
-Results are sorted by states.id in ascending order
-"""
+"""Script that lists all states with a name starting with N"""
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
-if __name__ == "__main__":
-    # Get MySQL credentials from command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-
-    # Create a cursor and execute the query
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all results and print each row
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    for state in cursor.fetchall():
+        print(state)
 
-    # Close cursor and connection
     cursor.close()
     db.close()
