e095f5c537839eafcc0a111ae883a857528be37f
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 21:26:08 +0100

    "feat: add ORM scripts and database filtering - cities, states, models"

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
 """
-Module for fetching a state by name.
+Script that prints the State object
+with the name passed as argument from
+the database hbtn_0e_6_usa
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-
+import sys
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 
 if __name__ == "__main__":
-
-    # Engine creation with mysql and mysqldb DBAPI
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
-    # Creating Session and its instance
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    try:
-        # The Query
-        state = session.query(State).filter(State.name == argv[4]).first()
-
-        # Printing the result
-        if state is None:
-            print("Not found")
-        else:
-            print(state.id)
-    finally:
-        # Closing the session
-        session.close()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
