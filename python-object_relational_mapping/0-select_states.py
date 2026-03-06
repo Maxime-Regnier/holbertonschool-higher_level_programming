c64751b0833d15aa49b808e551ac57bed04ed4a4
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 22:33:15 +0100

    Improve: Add explicit port 3306 and UTF-8 charset to MySQL connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3]
+        db=argv[3],
+        charset="utf8"
     )
     cursor = db.cursor()
 
