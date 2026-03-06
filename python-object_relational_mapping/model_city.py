31b58f3a34a6e7a1d6fb9e15c164b70b81782dc9
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 20:55:16 +0100

    Correction de model_city.py pour partager Base avec model_state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Module containing City class"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
