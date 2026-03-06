1b246282be684996c92ef029c15fc93bce16a6d4
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 22:20:17 +0100

    "feat: add 17-genre_life.sql - count shows by genre"

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT genres.name as genre, COUNT(tv_shows.id) as number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name as genre, COUNT(tv_shows.id) as number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY genres.id, genres.name
+GROUP BY tv_genres.id, tv_genres.name
 HAVING COUNT(tv_shows.id) > 0
 ORDER BY number_of_shows DESC;
