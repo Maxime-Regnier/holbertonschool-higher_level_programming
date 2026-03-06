4653066820608ffe70f177fe081516b8088dbe4c
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 11:46:19 +0100

    Add script to list all states from MySQL database using MySQLdb

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa
-Usage: ./0-select_states.py <username> <password> <database>
-Results are sorted by states.id in ascending order
-"""
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
@@ -13,20 +9,20 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    
+    # Connexion à la base
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=username, passwd=password,
+                           db=database, charset="utf8")
+    cur = conn.cursor()
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    # Exécuter la requête
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Create a cursor and execute query
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-
-    # Fetch all results and print them
-    rows = cursor.fetchall()
-    for row in rows:
+    # Afficher les résultats
+    for row in cur.fetchall():
         print(row)
 
-    # Close cursor and connection
-    cursor.close()
-    db.close()
+    # Fermer la connexion
+    cur.close()
+    conn.close()
