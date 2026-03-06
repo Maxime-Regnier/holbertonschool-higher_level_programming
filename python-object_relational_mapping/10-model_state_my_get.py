272ada002abd8ea94732552282c9ac275859e95d
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 21:37:20 +0100

    "feat: add 10-model_state_my_get.py"

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """
-Script that prints the State object
-with the name passed as argument from
-the database hbtn_0e_6_usa
+Module for fetching a state by name.
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+
+    # Engine creation with mysql and mysqldb DBAPI
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # Creating Session and its instance
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-    print("Not found" if not state else state.id)
+    try:
+        # The Query
+        state = session.query(State).filter(State.name == argv[4]).first()
+
+        # Printing the result
+        if state is None:
+            print("Not found")
+        else:
+            print(state.id)
+    finally:
+        # Closing the session
+        session.close()
