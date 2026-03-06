c2719a740d23cbfe08dbfbb86cafee1fc41bd212
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-06 20:58:01 +0100

    Ajout de # type: ignore pour ignorer l'erreur d'importation SQLAlchemy

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module containing City class"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey  # type: ignore
 from model_state import Base
 
 
