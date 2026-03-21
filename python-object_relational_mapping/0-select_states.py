8db547a368976d588235cd2630bef87db9f61bd8
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-21 12:25:54 +0100

    Add select states ORM script

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-"""Displays all states from the database hbtn_0e_0_usa"""
+"""List all states from a MySQL database."""
 
-if __name__ == '__main__':
-    from sys import argv
+
+def main():
+    """Connect to the database and print all states ordered by id."""
     import MySQLdb
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=username,
+        passwd=password,
+        db=database,
         charset="utf8"
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
     for state in cursor.fetchall():
         print(state)
+
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
