b914dc35b738c6f837fd088afe3e6e8edb383027
Maxime-Regnier <11493@holbertonstudents.com>
2026-03-27 10:43:06 +0100

    Update DOM manipulation task 3 script

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,6 +1,10 @@
-document.querySelector('#toggle_header').addEventListener('click', function () {
-  const header = document.querySelector('header');
-
-  header.classList.toggle('red');
-  header.classList.toggle('green');
+document.querySelector('#toggle_header').addEventListener('click', function() {
+const header = document.querySelector('header');
+if (header.classList.contains('red')) {
+header.classList.remove('red');
+header.classList.add('green');
+} else {
+    header.classList.remove('green');
+    header.classList.add('red');
+}
 });
